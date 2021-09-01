ddee8e2ecef339ab5fa2579f07ccf1d679bff472
Hyunsoo-Ryan-Lee <83285291+Hyunsoo-Ryan-Lee@users.noreply.github.com>
2021-09-02 02:08:48 +0900

    bmi calculator finish

--- bmi_calculator/lib/main.dart
+++ bmi_calculator/lib/main.dart
@@ -23,6 +23,11 @@ class BmiCalculator extends StatefulWidget {
 
 class _BmiCalculatorState extends State<BmiCalculator> {
   int currentindex = 0;
+  String result = '';
+  double height = 0;
+  double weight = 0;
+  TextEditingController heightcontroller = TextEditingController();
+  TextEditingController weightcontroller = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
@@ -49,19 +54,133 @@ class _BmiCalculatorState extends State<BmiCalculator> {
           child: Padding(
             padding: const EdgeInsets.all(12.0),
             child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.start,
               children: [
                 Row(
                   children: [
-                    radioButton('Man', Colors.blue, 0),
-                    radioButton('Women', Colors.pink, 1)
+                    radioButton('Man', Colors.blueAccent, 0),
+                    radioButton('Women', Colors.redAccent, 1)
                   ],
-                )
+                ),
+                SizedBox(
+                  height: 20.0,
+                ),
+                Text(
+                  'Your Height in cm :',
+                  style: TextStyle(
+                    fontSize: 18.0,
+                  ),
+                ),
+                SizedBox(
+                  height: 8.0,
+                ),
+                TextField(
+                  keyboardType: TextInputType.number,
+                  controller: heightcontroller,
+                  textAlign: TextAlign.center, // 내부 문자 center 정렬
+                  decoration: InputDecoration(
+                      hintText: 'Your Height in cm', // 입력칸에 연하게 보이는 문자
+                      filled: true,
+                      fillColor: Colors.grey[200],
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(8.0),
+                        borderSide: BorderSide.none,
+                      )),
+                ),
+                SizedBox(
+                  height: 20.0,
+                ),
+                Text(
+                  'Your Weight in Kg :',
+                  style: TextStyle(
+                    fontSize: 18.0,
+                  ),
+                ),
+                SizedBox(
+                  height: 8.0,
+                ),
+                TextField(
+                  keyboardType: TextInputType.number,
+                  controller: weightcontroller,
+                  textAlign: TextAlign.center, // 내부 문자 center 정렬
+                  decoration: InputDecoration(
+                      hintText: 'Your Weight in Kg', // 입력칸에 연하게 보이는 문자
+                      filled: true,
+                      fillColor: Colors.grey[200],
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(8.0),
+                        borderSide: BorderSide.none,
+                      )),
+                ),
+                SizedBox(
+                  height: 20.0,
+                ),
+                Container(
+                    width: double.infinity,
+                    height: 50.0,
+                    child: ElevatedButton(
+                      onPressed: () {
+                        setState(() {
+                          height = double.parse(heightcontroller.value.text);
+                          weight = double.parse(weightcontroller.value.text);
+                        });
+                        calculateBmi(height, weight);
+                      },
+                      style: TextButton.styleFrom(primary: Colors.blue[200]),
+                      child: Text(
+                        'Calculate',
+                        style: TextStyle(
+                          color: Colors.white,
+                        ),
+                      ),
+                    )),
+                SizedBox(
+                  height: 20.0,
+                ),
+                Container(
+                  width: double.infinity,
+                  child: Text(
+                    'Your BMI is : ',
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      fontSize: 24.0,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+                SizedBox(height: 50.0),
+                Container(
+                  width: double.infinity,
+                  child: Text(
+                    '$result',
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      fontSize: 40.0,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
               ],
             ),
           ),
         ),
       ),
     );
+  }
+
+  // String calculateBmi(double height, double weight) {
+  //   double result = weight / (height * height);
+  //   String bmi = result.toStringAsFixed(2);
+  //   return bmi;
+  // }
+
+  void calculateBmi(double height, double weight) {
+    double finalresult = weight / (height * height / 10000);
+    String bmi = finalresult.toStringAsFixed(2);
+    setState(() {
+      result = bmi;
+    });
   }
 
   void change(int index) {
@@ -74,13 +193,16 @@ class _BmiCalculatorState extends State<BmiCalculator> {
     return Expanded(
       child: Container(
         margin: EdgeInsets.symmetric(horizontal: 12.0),
+        height: 80.0,
         child: OutlinedButton(
-          style: ButtonStyle(
-              backgroundColor: MaterialStateProperty.all(Colors.grey[100])),
-          // color: currentindex == index ? color : Colors.grey[200],
-          // shape: RoundedRectangleBorder(
-          //   borderRadius: BorderRadius.circular(8.0),
-          // ),
+          // style: ButtonStyle(
+          // backgroundColor: MaterialStateProperty.all(Colors.grey[100])),
+          style: OutlinedButton.styleFrom(
+            backgroundColor: currentindex == index ? color : Colors.grey[200],
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(8.0),
+            ),
+          ),
           onPressed: () {
             change(index);
           },
@@ -88,6 +210,8 @@ class _BmiCalculatorState extends State<BmiCalculator> {
             value,
             style: TextStyle(
               color: currentindex == index ? Colors.black : color,
+              fontSize: 20.0,
+              fontWeight: FontWeight.bold,
             ),
           ),
         ),
